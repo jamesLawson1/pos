371193b208e4560223388458f1308cec389eb592
jamesLawson1 <jaylaw100.jl@gmail.com>
2025-02-08 11:59:25 +0000

    database migrations and additional fields on register page

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username');
+            $table->string('phone');
+            $table->string('photo');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
